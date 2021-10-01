74ed5ecd8063d30acd290a4e8bfb80d418642206
soukainahikma <soukainahikma@gmail.com>
2021-10-01 08:32:55 +0100

    test

--- main.cpp
+++ main.cpp
@@ -13,10 +13,15 @@ int main ()
 		test.push_back(i);
 	}
 	ft::vector<int> a(3,3);
-	ft::vector<int>::iterator itb = a.begin();
-	ft::vector<int>::iterator ite = a.end();
+	for (size_t i = 0; i < 3; i++)
+	{
+		a[i] = a[i] + i;
+	}
+	ft::vector<int>::iterator itb = a.rbegin();
+	ft::vector<int>::iterator ite = a.rend();
 	std::cout << "|begin()|"<< *itb << std::endl;
-	std::cout << "|  end()|"<< *ite << std::endl;
+	std::cout << "|end()  |"<< *ite << std::endl;
+	std::cout << "|Allocation max size|"<<test.max_size();
 
 	
 }
--- vector.hpp
+++ vector.hpp
@@ -7,16 +7,16 @@ namespace ft
 {
 	template <typename T>
 	class vector
-	{	
-
+	{
 		private:
-		 std::allocator<T> dataAlloc;
-		 T *vec;
-		 int _number_of_elements;
+			std::allocator<T> dataAlloc;
+			T *vec;
+			int _number_of_elements;
 		public:
 			vector(){} // default
 			vector(int number_of_elements , T val)// parametrized 
 			{
+				std::cout << "|allocation max size|" << dataAlloc.max_size() <<std::endl;
 				_number_of_elements = number_of_elements;
 				for (int i = 0; i < number_of_elements; i++)
 					vec[i] = val;
@@ -27,12 +27,20 @@ namespace ft
 				/* This destroys all container elements, and deallocates all the storage capacity allocated by the vector using its allocator. */
 			}
 			typedef _iterator<T> iterator;
-		typedef const _iterator<T> const_iterator;
+			typedef const _iterator<T> const_iterator;
+			typedef const _reverseiterator<T> reverse_iterator; 
+			typedef const _reverseiterator<T> const_reverse_iterator;
+			typedef size_t size_type;
 			iterator begin(){return(iterator(&vec[0]));};
 			iterator end(){return(iterator(&vec[_number_of_elements - 1]));};
 			const_iterator begin() const {return(const_iterator(&vec[0]));};
 			const_iterator end() const {return(const_iterator(&vec[_number_of_elements - 1]));};
-
+			reverse_iterator rend(){return(reverse_iterator(&vec[0]));};
+			reverse_iterator rbegin(){return(reverse_iterator(&vec[_number_of_elements - 1]));};
+			const_reverse_iterator rend()const {return(const_reverse_iterator(&vec[0]));};
+			const_reverse_iterator rbegin()const {return(const_reverse_iterator(&vec[_number_of_elements - 1]));};
+			size_type size() const{return(_number_of_elements);};
+			size_type max_size() const{return(dataAlloc.max_size());};
 			//operators
 			T &operator[](unsigned int i){
 				// if (i >= ve || i < 0)
